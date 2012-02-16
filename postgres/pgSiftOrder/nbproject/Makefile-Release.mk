94b07e399058e9b1e597f532d566171813745391
Petr Chmelar <chmelarp@fit.vutbr.cz>
2012-02-16 17:51:19 +0100

    PostgreSQL extensions added

---
+++ postgres/pgSiftOrder/nbproject/Makefile-Release.mk
@@ -0,0 +1,67 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=
+
+# Macros
+PLATFORM=GNU-Linux-x86
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=build/Release/${PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/pgsiftorder.o
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	${MAKE}  -f nbproject/Makefile-Release.mk dist/Release/${PLATFORM}/libpgSiftOrder.so
+
+dist/Release/${PLATFORM}/libpgSiftOrder.so: ${OBJECTFILES}
+	${MKDIR} -p dist/Release/${PLATFORM}
+	${LINK.c} -shared -o dist/Release/${PLATFORM}/libpgSiftOrder.so -fPIC ${OBJECTFILES} ${LDLIBSOPTIONS} 
+
+${OBJECTDIR}/pgsiftorder.o: pgsiftorder.c 
+	${MKDIR} -p ${OBJECTDIR}
+	$(COMPILE.c) -O2 -I`pg_config --includedir-server` -I`pg_config --includedir` -fPIC  -o ${OBJECTDIR}/pgsiftorder.o pgsiftorder.c
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf:
+	${RM} -r build/Release
+	${RM} dist/Release/${PLATFORM}/libpgSiftOrder.so
+
+# Subprojects
+.clean-subprojects:
