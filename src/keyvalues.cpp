9a4fe45d443ebe605a9130680af9876acae457d3
Volf Tomas <ivolf@vidte.fit.vutbr.cz>
2015-02-06 17:33:12 +0100

    Forgotten DoxyLayout file to commit

--- src/keyvalues.cpp
+++ src/keyvalues.cpp
@@ -1,5 +1,5 @@
 /**
- * @file    commons.cpp
+ * @file    keyvalues.cpp
  * @author  VTApi Team, FIT BUT, CZ
  * @author  Petr Chmelar, chmelarp@fit.vutbr.cz
  * @author  Vojtech Froml, xfroml00@stud.fit.vutbr.cz
