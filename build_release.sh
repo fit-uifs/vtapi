5a94fd3dad208f5ecdae6fee63c9f7cf9a687847
vlczech <ivolf@fit.vutbr.cz>
2015-09-18 06:07:37 +0200

    CMake: variable component paths

--- build_release.sh
+++ build_release.sh
@@ -1,17 +1,33 @@
 #! /usr/bin/env bash
 
-# where to install executables, libraries and headers
-# (relative path to ./build or absolute)
-INSTALL_PREFIX="/usr/local"
-# where to install module libraries
-# (relative path to INSTALL_PREFIX or absolute)
-MODULES_DIR="lib"
+# Usage: ./build_release [-D<variable>=<value>]
+#
+# Path customization variables:
+#   * POCO_PATH: path to root dir of Poco project (where are dirs "include", "lib", ...)
+#   * POCO_INCLUDE_PATH: path to include dir of Poco project
+#   * POCO_LIBRARY_PATH: path to library dir of Poco project
+#   * POCOFOUNDATION_INCLUDE_PATH, POCOUTIL_INCLUDE_PATH, POCOXML_INCLUDE_PATH:
+#          path to include dir of individual parts of Poco project
+#   * POCOFOUNDATION_LIBRARY_PATH, POCOUTIL_LIBRARY_PATH, POCOXML_LIBRARY_PATH:
+#          path to library dir of individual parts of Poco project
+#
+#   * OPENCV_PATH: path in which OpenCV package could be found
+#   * OPENCV_PC_PATH: path to opencv.pc file for pkg-config (including file name)
+#
+#   * PG_CONFIG_PATH: path to pg_config executable (including file name)
+#   * LIBPQTYPES_PATH: path to root dir of PQtypes library (where are dirs "include", "lib", ...)
+#
+#   * SQLITE3_PC_PATH: path to sqlite3.pc file for pkg-config (including file name)
 
 
 mkdir -p build/release
-mkdir -p $INSTALL_PREFIX
 cd build/release
-cmake ../.. -DCMAKE_BUILD_TYPE:STRING="Release" -DCMAKE_INSTALL_PREFIX:PATH=$INSTALL_PREFIX -DVTAPI_MODULES_DIR=$MODULES_DIR
-make
-#make install
-sudo make install
+cmake ../.. -Wno-dev -DPOCO_PATH=/home/vlczech/Plocha/poco-1.6.1
+if test $? = 0
+then
+  make
+  if test $? = 0
+  then
+    sudo make install
+  fi
+fi
